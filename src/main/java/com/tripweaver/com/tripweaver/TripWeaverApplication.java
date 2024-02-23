bf1f0bbdc72fc251139b985a43d6a3e85e8bf5d5
Yulian Kremenski <yulian.kremenski@yahoo.com>
2024-02-23 10:22:30 +0200

    Initializing the repository.

---
+++ src/main/java/com/tripweaver/com/tripweaver/TripWeaverApplication.java
@@ -0,0 +1,13 @@
+package com.tripweaver.com.tripweaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripWeaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripWeaverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tripweaver/com/tripweaver/TripWeaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripweaver.com.tripweaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripWeaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
