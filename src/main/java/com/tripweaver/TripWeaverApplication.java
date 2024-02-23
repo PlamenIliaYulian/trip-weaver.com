88da281243a6502704815a54577a34f6fa1874ea
ilia Iliev <ilia.dimchev.iliev@gmail.com>
2024-02-23 12:25:52 +0200

    - Build.gradle dependencies added. - Application.properties configurations added. - HibernateConfig class implemented.

--- src/main/java/com/tripweaver/TripWeaverApplication.java
+++ src/main/java/com/tripweaver/TripWeaverApplication.java
@@ -1,4 +1,4 @@
-package com.tripweaver.com.tripweaver;
+package com.tripweaver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/tripweaver/models/User.java
@@ -0,0 +1,4 @@
+package com.tripweaver.models;
+
+public class User {
+}
