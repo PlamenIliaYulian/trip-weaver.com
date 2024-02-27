cd7700d94c60fd0a1f8cbf302b26a6abfa4392ad
Yulian Kremenski <yulian.kremenski@yahoo.com>
2024-02-27 08:20:08 +0200

    - Implemented getters and setters in the TravelDto.java class; - Implemented the UserDto.java class;

--- src/main/java/com/tripweaver/models/dtos/UserDto.java
+++ src/main/java/com/tripweaver/models/dtos/UserDto.java
@@ -1,5 +1,9 @@
 package com.tripweaver.models.dtos;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
@@ -22,6 +26,10 @@ public class UserDto {
     private String lastName;
     @NotNull
     @Email
+    @Pattern(
+            regexp = "^(?!.*[?:{}])[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
+            message = "Email must be a valid email. It cannot contain '?', ':', '!', '}', '{'.")
+
     private String email;
     @NotNull
     @Size(min = 10, max = 10, message = "Phone number must be 10 digits starting with 08.")
