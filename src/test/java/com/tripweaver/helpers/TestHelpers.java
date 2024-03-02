c8e5bf001a0e6917f3f9c48f52911e5c675b4d6f
ilia Iliev <ilia.dimchev.iliev@gmail.com>
2024-03-02 17:42:04 +0200

    - Tested Ilia's methods in AvatarServiceImpl.

--- src/test/java/com/tripweaver/helpers/TestHelpers.java
+++ src/test/java/com/tripweaver/helpers/TestHelpers.java
@@ -1,7 +1,13 @@
 package com.tripweaver.helpers;
 
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
 public class TestHelpers {
 
+    public static MultipartFile createMockMultipartFile() {
+        return new MockMultipartFile("String",new byte[10]);
+    }
 
 
 }
--- src/test/java/com/tripweaver/services/AvatarServiceTests.java
+++ src/test/java/com/tripweaver/services/AvatarServiceTests.java
@@ -1,10 +1,15 @@
 package com.tripweaver.services;
 
+import com.tripweaver.helpers.TestHelpers;
 import com.tripweaver.repositories.contracts.AvatarRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 
 @ExtendWith(MockitoExtension.class)
 public class AvatarServiceTests {
@@ -14,5 +19,25 @@ public class AvatarServiceTests {
 
     @InjectMocks
     AvatarServiceImpl avatarService;
+
+    /*Ilia*/
+    @Test
+    public void uploadPictureToCloudinary_Should_CallRepository() {
+        MultipartFile mockMultipartFile= TestHelpers.createMockMultipartFile();
+
+        avatarService.uploadPictureToCloudinary(mockMultipartFile);
+
+        Mockito.verify(avatarRepository,Mockito.times(1))
+                .uploadPictureToCloudinary(mockMultipartFile);
+    }
+    /*Ilia*/
+    @Test
+    public void getAvatarById_Should_CallRepository() {
+        avatarService.getAvatarById(Mockito.anyInt());
+
+        Mockito.verify(avatarRepository,Mockito.times(1))
+                .getAvatarById(Mockito.anyInt());
+    }
+
 
 }
