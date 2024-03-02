52e462f1ee34a8a76e0eff7b8ae57dd968dac121
ilia Iliev <ilia.dimchev.iliev@gmail.com>
2024-03-02 17:48:00 +0200

    - Tested Ilia's methods in RoleServiceImpl.

--- src/test/java/com/tripweaver/services/RoleServiceTests.java
+++ src/test/java/com/tripweaver/services/RoleServiceTests.java
@@ -2,9 +2,11 @@ package com.tripweaver.services;
 
 import com.tripweaver.repositories.contracts.RoleRepository;
 import com.tripweaver.services.contracts.RoleService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -16,4 +18,12 @@ public class RoleServiceTests {
 
     @InjectMocks
     RoleServiceImpl roleService;
+
+    @Test
+    public void getRoleById_Should_CallRepository() {
+        roleService.getRoleById(Mockito.anyInt());
+
+        Mockito.verify(roleRepository,Mockito.times(1))
+                .getRoleById(Mockito.anyInt());
+    }
 }
